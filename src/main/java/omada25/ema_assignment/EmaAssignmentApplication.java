025a3cc7f19385e304740f5346e86b4faaf87d57
Alexpapag13 <it2021072@hua.gr>
2023-12-11 13:50:04 +0200

    Initial commit

---
+++ src/main/java/omada25/ema_assignment/EmaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package omada25.ema_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmaAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/omada25/ema_assignment/EmaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package omada25.ema_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
