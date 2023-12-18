b0676851a410328fcb303d119581e732acb0c7f8
Alexpapag13 <it2021072@hua.gr>
2023-12-18 14:08:25 +0200

    peiramatizomai

--- src/main/java/omada25/ema_assignment/EmaAssignmentApplication.java
+++ src/main/java/omada25/ema_assignment/EmaAssignmentApplication.java
@@ -1,9 +1,12 @@
 package omada25.ema_assignment;
 
+import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class EmaAssignmentApplication {
 
     public static void main(String[] args) {
